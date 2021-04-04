31b1d86a720e0bc1e1023fab23f035d298274dca
Tony <Zylphrex@gmail.com>
2021-04-04 00:00:11 +0000

    use state type

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,27 +3,26 @@ module Parser where
 import Control.Applicative ( Alternative((<|>), empty) )
 import Data.Char ( isSpace )
 
-newtype Parser a = Parser
-    { runParser :: String -> Maybe (a, String)
-    }
+newtype ParserState = ParserState
+    { buffer :: String
+    } deriving Show
 
-executeParser :: Parser a -> String -> Maybe a
-executeParser p input = do
-    (a, input) <- runParser p input
-    if null input then pure a else empty
+newtype Parser a = Parser
+    { runParser :: ParserState -> Maybe (a, ParserState)
+    }
 
 instance Functor Parser where
     fmap f (Parser p) = Parser $ fmap g . p
-        where g (x, input) = (f x, input)
+        where g (x, state) = (f x, state)
 
 instance Applicative Parser where
     pure x = Parser p
-        where p input = pure (x, input)
+        where p state = pure (x, state)
     (Parser ap) <*> (Parser p) = Parser q
-        where q input = do
-                (f, input) <- ap input
-                (a, input) <- p input
-                pure (f a, input)
+        where q state = do
+                (f, state) <- ap state
+                (a, state) <- p state
+                pure (f a, state)
 
 instance Alternative Parser where
     empty = Parser $ const empty
@@ -32,17 +31,30 @@ instance Alternative Parser where
 
 instance Monad Parser where
     (Parser p1) >>= mp = Parser q
-        where q input = do
-                (a, input) <- p1 input
-                runParser (mp a) input
+        where q state = do
+                (a, state) <- p1 state
+                runParser (mp a) state
+
+
+executeParser :: Parser a -> String -> Maybe a
+executeParser p s = do
+    (a, state) <- runParser p $ ParserState { buffer = s}
+    if null (buffer state) then pure a else empty
+
+mapSnd :: (b -> c) -> (a, b) -> (a, c)
+mapSnd f (a, b) = (a, f b)
+
+fromTuple :: (a, String) -> (a, ParserState)
+fromTuple = mapSnd ParserState
 
 parseIf :: (Char -> Bool) -> Parser Char
 parseIf predicate = Parser p
-    where p (x:xs) | predicate x = pure (x, xs)
-          p _                    = empty
+    where p (ParserState (x:xs)) | predicate x = pure $ fromTuple (x, xs)
+          p _                                  = empty
 
 parseWhile :: (Char -> Bool) -> Parser String
-parseWhile predicate = Parser $ pure . span predicate
+parseWhile predicate = Parser p
+    where p = pure . fromTuple . span predicate . buffer
 
 parseCharacter :: Char -> Parser Char
 parseCharacter c = parseIf (== c)
