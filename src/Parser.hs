0abf5297bfc71dd31f0d8a1b481fa069b259c3b3
Tony <Zylphrex@gmail.com>
2021-04-04 02:07:01 +0000

    use StateT Transformer

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,60 +1,56 @@
 module Parser where
 
 import Control.Applicative ( Alternative((<|>), empty) )
+import Control.Monad.State ( StateT(StateT), evalStateT, mapStateT, runStateT )
 import Data.Char ( isSpace )
 
 newtype ParserState = ParserState
     { buffer :: String
     } deriving Show
 
+instance Read ParserState where
+    readsPrec _ s = [(ParserState { buffer = s }, "")]
+
 newtype Parser a = Parser
-    { runParser :: ParserState -> Maybe (a, ParserState)
+    { runParser :: StateT ParserState Maybe a
     }
 
 instance Functor Parser where
-    fmap f (Parser p) = Parser $ fmap g . p
-        where g (x, state) = (f x, state)
+    f `fmap` (Parser p) = Parser $ f <$> p
 
 instance Applicative Parser where
-    pure x = Parser p
-        where p state = pure (x, state)
-    (Parser ap) <*> (Parser p) = Parser q
-        where q state = do
-                (f, state) <- ap state
-                (a, state) <- p state
-                pure (f a, state)
+    pure x = Parser $ pure x
+    (Parser ap) <*> (Parser p) = Parser $ ap <*> p
 
 instance Alternative Parser where
-    empty = Parser $ const empty
-    (Parser p1) <|> (Parser p2) = Parser p
-        where p input = p1 input <|> p2 input
+    empty = Parser $ StateT $ pure empty
+    (Parser p1) <|> (Parser p2) = Parser $ p1 <|> p2
 
 instance Monad Parser where
-    (Parser p1) >>= mp = Parser q
+    (Parser p) >>= mp = Parser $ StateT q
         where q state = do
-                (a, state) <- p1 state
-                runParser (mp a) state
-
+                (a, state) <- runStateT p state
+                runStateT (runParser (mp a)) state
 
 executeParser :: Parser a -> String -> Maybe a
-executeParser p s = do
-    (a, state) <- runParser p $ ParserState { buffer = s}
+executeParser (Parser p) s = do
+    (a, state) <- runStateT p $ read s
     if null (buffer state) then pure a else empty
 
 mapSnd :: (b -> c) -> (a, b) -> (a, c)
 mapSnd f (a, b) = (a, f b)
 
-fromTuple :: (a, String) -> (a, ParserState)
-fromTuple = mapSnd ParserState
+fromPair :: (a, String) -> (a, ParserState)
+fromPair = mapSnd ParserState
 
 parseIf :: (Char -> Bool) -> Parser Char
-parseIf predicate = Parser p
-    where p (ParserState (x:xs)) | predicate x = pure $ fromTuple (x, xs)
+parseIf predicate = Parser $ StateT p
+    where p (ParserState (x:xs)) | predicate x = pure $ fromPair (x, xs)
           p _                                  = empty
 
 parseWhile :: (Char -> Bool) -> Parser String
-parseWhile predicate = Parser p
-    where p = pure . fromTuple . span predicate . buffer
+parseWhile predicate = Parser $ StateT p
+    where p = pure . fromPair . span predicate . buffer
 
 parseCharacter :: Char -> Parser Char
 parseCharacter c = parseIf (== c)
