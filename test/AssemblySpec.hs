242c4e1f7af29295abf718d901ca4b0b769ee81a
Tony X <zylphrex@gmail.com>
2021-04-12 23:38:58 -0400

    remove redundant dos

--- test/AssemblySpec.hs
+++ test/AssemblySpec.hs
@@ -20,96 +20,95 @@ import Assembly ( OsOption(Darwin, Other)
                 )
 
 spec :: Spec
-spec = do
-    describe "Assembly" $ do
-        describe "Darwin" $ do
-            let option = Option { osOption = Darwin}
+spec = describe "Assembly" $ do
+    describe "Darwin" $ do
+        let option = Option { osOption = Darwin}
 
-            it "translates simple program" $ do
-                let function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return $ Int32 124
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tretq\n"
+        it "translates simple program" $ do
+            let function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return $ Int32 124
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tretq\n"
 
-            it "translates programs with negation" $ do
-                let expression = UnaryExpression Negation $ Int32 124
-                    function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return expression
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tneg\t%eax\n\tretq\n"
+        it "translates programs with negation" $ do
+            let expression = UnaryExpression Negation $ Int32 124
+                function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return expression
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tneg\t%eax\n\tretq\n"
 
-            it "translates programs with bitwise complement" $ do
-                let expression = UnaryExpression BitwiseComplement $ Int32 124
-                    function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return expression
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tnot\t%eax\n\tretq\n"
+        it "translates programs with bitwise complement" $ do
+            let expression = UnaryExpression BitwiseComplement $ Int32 124
+                function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return expression
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tnot\t%eax\n\tretq\n"
 
-            it "translates programs with logical negation" $ do
-                let expression = UnaryExpression LogicalNegation $ Int32 124
-                    function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return expression
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tcmpl\t$0, %eax\n\tsete\t%al\n\tretq\n"
+        it "translates programs with logical negation" $ do
+            let expression = UnaryExpression LogicalNegation $ Int32 124
+                function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return expression
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\t_main\n_main:\n\tmovl\t$124, %eax\n\tcmpl\t$0, %eax\n\tsete\t%al\n\tretq\n"
 
-        describe "Other" $ do
-            let option = Option { osOption = Other }
+    describe "Other" $ do
+        let option = Option { osOption = Other }
 
-            it "translates simple program" $ do
-                let function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return $ Int32 124
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tretq\n"
+        it "translates simple program" $ do
+            let function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return $ Int32 124
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tretq\n"
 
-            it "translates programs with negation" $ do
-                let expression = UnaryExpression Negation $ Int32 124
-                    function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return expression
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tneg\t%eax\n\tretq\n"
+        it "translates programs with negation" $ do
+            let expression = UnaryExpression Negation $ Int32 124
+                function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return expression
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tneg\t%eax\n\tretq\n"
 
-            it "translates programs with bitwise complement" $ do
-                let expression = UnaryExpression BitwiseComplement $ Int32 124
-                    function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return expression
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tnot\t%eax\n\tretq\n"
+        it "translates programs with bitwise complement" $ do
+            let expression = UnaryExpression BitwiseComplement $ Int32 124
+                function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return expression
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tnot\t%eax\n\tretq\n"
 
-            it "translates programs with logical negation" $ do
-                let expression = UnaryExpression LogicalNegation $ Int32 124
-                    function = Function { returnType = Int
-                                        , identifier = "main"
-                                        , arguments  = ()
-                                        , body       = Return expression
-                                        }
-                    program  = Program function
-                    assembly = asAssembly option program
-                assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tcmpl\t$0, %eax\n\tsete\t%al\n\tretq\n"
+        it "translates programs with logical negation" $ do
+            let expression = UnaryExpression LogicalNegation $ Int32 124
+                function = Function { returnType = Int
+                                    , identifier = "main"
+                                    , arguments  = ()
+                                    , body       = Return expression
+                                    }
+                program  = Program function
+                assembly = asAssembly option program
+            assembly `shouldBe` "\t.globl\tmain\nmain:\n\tmovl\t$124, %eax\n\tcmpl\t$0, %eax\n\tsete\t%al\n\tretq\n"
